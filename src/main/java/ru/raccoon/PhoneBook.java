c971cd9dd8332643772c7acbfb5f373b8e7dac05
sserh <serhios@bk.ru>
2023-12-01 10:02:39 +0700

    Stub and tests for add

--- src/main/java/ru/raccoon/PhoneBook.java
+++ src/main/java/ru/raccoon/PhoneBook.java
@@ -1,4 +1,7 @@
 package ru.raccoon;
 
 public class PhoneBook {
+    public int add(String name, String phoneNumber) {
+        return 0;
+    }
 }
--- src/test/java/ru/raccoon/PhoneBookTest.java
+++ src/test/java/ru/raccoon/PhoneBookTest.java
@@ -1,5 +1,14 @@
 package ru.raccoon;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class PhoneBookTest {
 
+    @Test
+    void add() {
+        PhoneBook phoneBook = new PhoneBook();
+        Assertions.assertEquals(1, phoneBook.add("TestName_1", "PhoneNumber_1"));
+        Assertions.assertEquals(2, phoneBook.add("TestName_2", "PhoneNumber_2"));
+    }
 }
