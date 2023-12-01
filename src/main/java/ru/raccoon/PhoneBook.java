88b4ba8eaa0b814e4da37be06eda71f0a4baf974
sserh <serhios@bk.ru>
2023-12-01 10:07:17 +0700

    Add implemented

--- src/main/java/ru/raccoon/PhoneBook.java
+++ src/main/java/ru/raccoon/PhoneBook.java
@@ -1,7 +1,14 @@
 package ru.raccoon;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    Map<String, String> phoneBookMap = new HashMap<>();
     public int add(String name, String phoneNumber) {
-        return 0;
+        phoneBookMap.put(name, phoneNumber); //добавим запрошенный контакт в мапу
+        //вернём размер мапы
+        return phoneBookMap.size();
     }
 }
